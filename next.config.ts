a989a581602c5082bdfb13444db6754d58ed73c3
Targetpraks <rickymaio@gmail.com>
2026-05-02 22:51:03 +0200

    Add basePath for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "dist",
+  basePath: "/papa-pasta-fnd",
   images: {
     unoptimized: true,
   },
