a1568fa6d3a0b7bda59ef229c663ebc33278b579
Targetpraks <rickymaio@gmail.com>
2026-05-03 11:42:01 +0200

    build: remove basePath slash papa-pasta-fnd for root domain deploy

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "dist",
-  basePath: "/papa-pasta-fnd",
+  basePath: "",
   images: {
     unoptimized: true,
   },
